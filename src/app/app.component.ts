bf51c81a0345a5b2995d15173ab5f127224ee7e9
Ávilla Silva <silvaavilla74@gmail.com>
2024-10-02 21:57:26 -0300

    Add new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    UserComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
