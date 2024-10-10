d4722f2fdd00e2f62089c47822b8bc46db6be8ab
Ávilla Silva <silvaavilla74@gmail.com>
2024-10-09 21:59:01 -0300

    Introduce User inteface in UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,11 +15,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class UserComponent {
 
-  @Input({ required: true }) user!: {
-    id: string;
-    avatar: string;
-    name: string;
-  }
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter();
 
   get imagePath() {
