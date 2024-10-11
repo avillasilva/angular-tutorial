17da6bbe39d8ed52aa872f16096915d424077024
Ávilla Silva <silvaavilla74@gmail.com>
2024-10-10 21:47:18 -0300

    Introduce the task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+
+interface Task {
+  id: string;
+}
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
 
   @Input() name?: string;
+  tasks!: Task[];
 
 }
